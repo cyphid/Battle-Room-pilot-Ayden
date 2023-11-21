dc66d826e95141518a0f7ef71abc4155fb909043
cyphid <146806761+cyphid@users.noreply.github.com>
2023-11-21 05:44:27 +0000

    2023-11-14 submission

---
+++ ideas.md
@@ -0,0 +1,13 @@
+detect the proximity of nearby snakes to see if kills are viable
+
+3-4 move foresight, let it detect if its walking into a trap.
+- to do that, make the snake 'think' of likely patterns based on snake positions (if a snake is trying to intercept, estimate it may be setting a trap)
+
+Once we can defend against said attacks, let's try and execute them. 
+- chase your own tail once you get too long to carve out your section of board
+- cut off snakes that are running along the edges
+- trap other snakes
+- control the middle of the board
+- encircle short snakes
+
+But the summary of all these ideas is we want a snake that can THINK while following rules. A possible learning exercise may be putting these directives into a syntax, then asking an AI to convert them into notes!
--- main.py
+++ main.py
@@ -96,19 +96,6 @@ def move(game_state: typing.Dict) -> typing.Dict:
                 is_move_safe['right'] = False
             if segment['x'] == my_head['x'] - 1 and segment['y'] == my_head['y']:
                 is_move_safe['left'] = False
-  
-    
-    
-
-    # Are there any safe moves left?
-    safe_moves = []
-    for move, isSafe in is_move_safe.items():
-        if isSafe:
-            safe_moves.append(move)
-
-    if len(safe_moves) == 0:
-        print(f"MOVE {game_state['turn']}: No safe moves detected! Moving down")
-        return {"move": "down"}
 
     # TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
     food = game_state['board']['food']
@@ -167,7 +154,141 @@ def move(game_state: typing.Dict) -> typing.Dict:
         if dist < min_dist:
             min_dist = dist
             best_move = move
+
+
+    # ... The code before this remains unchanged
+
+    # Detect head-to-head collision possibilities
+    my_head = game_state['you']['body'][0]
+    my_length = game_state['you']['length']
+
+    # Check the neighbouring positions of the snake's head
+    possible_moves = {
+        "up": {"x": my_head['x'], "y": my_head['y'] + 1},
+        "down": {"x": my_head['x'], "y": my_head['y'] - 1},
+        "left": {"x": my_head['x'] - 1, "y": my_head['y']},
+        "right": {"x": my_head['x'] + 1, "y": my_head['y']}
+    }
+
+    # Look for other snake heads
+    for snake in game_state['board']['snakes']:
+        if snake['id'] == game_state['you']['id']:
+            continue  # Skip checking against our own snake's head
+
+        other_head = snake['head']
+        other_length = snake['length']
+
+        # Consider all potential moves of the other snake's head
+        other_possible_moves = [
+            {"x": other_head['x'] + 1, "y": other_head['y']},    # right
+            {"x": other_head['x'] - 1, "y": other_head['y']},    # left
+            {"x": other_head['x'], "y": other_head['y'] + 1},    # up
+            {"x": other_head['x'], "y": other_head['y'] - 1},    # down
+            # Diagonals
+            {"x": other_head['x'] + 1, "y": other_head['y'] + 1},  # up-right
+            {"x": other_head['x'] - 1, "y": other_head['y'] + 1},  # up-left
+            {"x": other_head['x'] + 1, "y": other_head['y'] - 1},  # down-right
+            {"x": other_head['x'] - 1, "y": other_head['y'] - 1},  # down-left
+        ]
+
+        # Check each of our possible moves
+        for my_move, my_new_head in possible_moves.items():
+            # If that move is already determined to be not safe, skip
+            if not is_move_safe[my_move]:
+                continue
+
+            # Check if the other snake could move to the same square
+            if my_new_head in other_possible_moves:
+                if other_length >= my_length:  # Other snake is bigger, treat as unsafe
+                    is_move_safe[my_move] = False
+                elif other_length < my_length:  # Other snake is smaller, can be aggressive
+                    # Decide if you want to move towards a smaller snake for a potential elimination
+                    # Or keep this as a safe move depending on your strategy
+                    pass  # Add your logic here
     
+    
+    my_head = game_state['you']['body'][0]
+    my_length = game_state['you']['length']
+
+    # Empty spaces between your snake's head and another snake's head means a potential collision
+    # Check two steps away in all directions
+    positions_two_steps_away = {
+        "up": {"x": my_head['x'], "y": my_head['y'] + 2},
+        "down": {"x": my_head['x'], "y": my_head['y'] - 2},
+        "left": {"x": my_head['x'] - 2, "y": my_head['y']},
+        "right": {"x": my_head['x'] + 2, "y": my_head['y']}
+    }
+
+    # Look for other snake heads that are one space away (meaning they could move to collide)
+    for snake in game_state['board']['snakes']:
+        if snake['id'] == game_state['you']['id']:
+            continue  # Skip our own snake
+
+        other_head = snake['head']
+        other_length = snake['length']
+
+        # Consider the move other snakes could make to reach a space one step from our head
+        for move, position in positions_two_steps_away.items():
+            if not is_move_safe[move]:  # If already unsafe, skip
+                continue
+
+            # Check if there's an enemy snake's head that could move into that position
+            if (abs(position['x'] - other_head['x']) <= 1) and \
+               (abs(position['y'] - other_head['y']) <= 1):
+                if other_length >= my_length:  # Other snake is equal or larger, don't risk collision
+                    is_move_safe[move] = False
+                else:  # Other snake is smaller, we could possibly take it out
+                    # Here you may choose to be aggressive or just keep this as a possible move
+                    pass  # Add your strategy logic for aggressive play here
+    
+    
+    # Current position of our snake's head
+    my_head = game_state['you']['body'][0]
+    my_length = game_state['you']['length']
+
+    # Get the other snakes
+    other_snakes = [snake for snake in game_state['board']['snakes'] if snake['id'] != game_state['you']['id']]
+
+    # Check the positions one space away diagonally from our head
+    diagonal_threats = [
+        {"x": my_head['x'] - 1, "y": my_head['y'] - 1},  # Up-left
+        {"x": my_head['x'] + 1, "y": my_head['y'] - 1},  # Up-right
+        {"x": my_head['x'] - 1, "y": my_head['y'] + 1},  # Down-left
+        {"x": my_head['x'] + 1, "y": my_head['y'] + 1},  # Down-right
+    ]
+
+    # Loop over each other snake's head
+    for other_snake in other_snakes:
+        other_head = other_snake['head']
+        other_length = other_snake['length']
+
+        # Calculate danger zones for each snake
+        danger_zones = [
+            {"x": other_head['x'] - 1, "y": other_head['y'] - 1},  # Up-left
+            {"x": other_head['x'] + 1, "y": other_head['y'] - 1},  # Up-right
+            {"x": other_head['x'] - 1, "y": other_head['y'] + 1},  # Down-left
+            {"x": other_head['x'] + 1, "y": other_head['y'] + 1},  # Down-right
+        ]
+
+        # Compare danger zones to our next potential head positions
+        for move, next_pos in possible_moves.items():
+            if next_pos in danger_zones:
+                # If this snake is bigger, we should be more cautious
+                if other_length >= my_length:
+                    is_move_safe[move] = False
+                # Optional: you could include else block here to enable aggressive behavior
+                # towards smaller snakes
+
+    # Are there any safe moves left?
+    safe_moves = []
+    for move, isSafe in is_move_safe.items():
+        if isSafe:
+            safe_moves.append(move)
+
+    if len(safe_moves) == 0:
+        print(f"MOVE {game_state['turn']}: No safe moves detected! Moving down")
+        return {"move": "down"}
+  
     if best_move in safe_moves:
       print(f"MOVE {game_state['turn']}: {best_move}")
       return {"move": best_move}
@@ -186,3 +307,4 @@ if __name__ == "__main__":
          "move": move, 
         "end": end
     })
+
